976cea24a9f0433c77692a96d9313f39f01763f8
Harshada Madhukar Shelar <harshadashelar274@gmail.com>
2026-09-08 08:54:14 +0000

    01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+accoundCity ="Jaipur"
+let accountState; 
+// accountId = 2 //not allowed
+
+accountEmail="hc@hc.com"
+accountPassword  = "21212121"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accoundCity,accountPassword,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope*/ 
